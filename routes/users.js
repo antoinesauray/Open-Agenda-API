46d8f1bd10c1be11b959f67e4ecdfe45cf862068
Antoine Sauray <sauray.antoine@gmail.com>
2016-10-03 18:20:58 +0200

    insert facebook token

--- routes/users.js
+++ routes/users.js
@@ -43,6 +43,9 @@ router.post('/', function(req, res, next) {
                 }
             ).spread(function(user, created) {
                 console.log("created: "+created);
+
+                database.query("UPDATE users set facebook_token=:fb_token where facebook_id=:fb_id RETURNING *", { replacements: { fb_token: req.body.access_token, fb_id: response.id }, type: database.QueryTypes.INSERT});
+
                 var token = jwt.sign({id: user.id }, credentials.key, { algorithm: 'RS256'});
                 if(created){
                     res.statusCode=201;
