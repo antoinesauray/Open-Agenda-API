d80bf8af47e71cd0d3ce8f962a964f0d1a283ba0
Antoine Sauray <sauray.antoine@gmail.com>
2016-10-15 21:59:37 +0200

    test

--- edt-query/query.js
+++ edt-query/query.js
@@ -220,6 +220,7 @@ module.exports = {
                 // get promises from all providers
                 var promises=[];
                 result.rows.forEach(function(agenda){
+                    console.log("agenda="+JSON.stringify(agenda));
                     var query=providers[agenda.provider].client.query("SELECT agenda_events.id, to_char(start_time, 'YYYY-MM-DD') AS date, start_time, end_time, name, event_type_id, color_light, color_dark, more FROM agenda_events LEFT JOIN event_types ON event_types.id=agenda_events.event_type_id where agenda_id=$1 AND start_time::date > $2 AND end_time::date <= $3", [agenda.id, start_date, end_date]);
                     query.then(function(){
                         providers[agenda.provider].done();
@@ -229,7 +230,6 @@ module.exports = {
                 // when we have all replies
                 when.all(promises).spread(function(results) {
                     var ret = {};
-                    console.log("results="+JSON.stringify(results));
                     if(results.rows){
                         results.rows.forEach(function(result){
                             result.rows.forEach(function(event){
