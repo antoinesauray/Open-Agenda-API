1e29ac477eefdb0204add04570cb1c1e0995b6be
Antoine Sauray <sauray.antoine@gmail.com>
2016-10-15 17:14:29 +0200

    log

--- edt-query/query.js
+++ edt-query/query.js
@@ -362,6 +362,7 @@ module.exports = {
                 }
                 console.log("verifying token");
                 central.provider.query("SELECT * from users where facebook_id=$1 OR facebook_email=$2", [response.id, response.email], function(err, result){
+                    console.log("done");
                     central.done();
                     if(err) {
                         console.log("token error");
