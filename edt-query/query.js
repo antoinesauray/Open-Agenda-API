201a58cee60a51f2bf58d2497a19b69ac7dcfb05
Antoine Sauray <sauray.antoine@gmail.com>
2016-10-16 11:13:52 +0200

    fix

--- edt-query/query.js
+++ edt-query/query.js
@@ -257,7 +257,7 @@ module.exports = {
                 var promises=[];
                 result.rows.forEach(function(agenda){
                     console.log("agenda: "+JSON.stringify(agenda));
-                    var sql = escape('select %I as provider', agenda.provider);
+                    var sql = escape('select %Q as provider', agenda.provider);
                     console.log("sql="+sql);
                     var query = providers[agenda.provider].client.query(sql+", id, name, editable, agenda_entity_id, agenda_type_id, more, active from agendas where id =$1", [agenda.agenda_id]);
                     promises.push(query);
