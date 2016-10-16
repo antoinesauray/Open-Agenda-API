42262e4e9bc0753ba845ded03eee311468207c3e
Antoine Sauray <sauray.antoine@gmail.com>
2016-10-16 14:14:55 +0200

    fix

--- edt-query/query.js
+++ edt-query/query.js
@@ -324,7 +324,7 @@ module.exports = {
             }
         },
         agendas: function(provider_id, agenda_id, user_id, res){
-            if(providers[provider]){
+            if(providers[provider_id]){
                 providers[provider_id].client.query("INSERT INTO user_agendas(created_at, updated_at, provider_id, agenda_id, user_id) VALUES(NOW(), NOW(), $1, $2, $3) RETURNING *", [provider_id, agenda_id, user_id], function(err, result){
                     providers[provider].done();
                     if(err) {
