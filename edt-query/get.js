e0710c28d7f430cc38543c24de48300fcef94eb3
Antoine Sauray <sauray.antoine@gmail.com>
2016-12-10 00:12:04 +0100

    fix

--- edt-query/get.js
+++ edt-query/get.js
@@ -60,7 +60,7 @@ module.exports = {
 
     notes: function(user_id, provider, event_id, res){
         if(query.getProviders()[provider]){
-            query.getProviders()[provider].client.query("SELECT message, first_name, last_name from user_notes JOIN users on user_id = edt_id where event_id = $1 AND (public=true OR (public=false AND user_id=$2))", [event_id, user_id], function(err, result){
+            query.getProviders()[provider].client.query("SELECT type, content, first_name, user_id, last_name, public, user_notes.created_at, user_notes.updated_at from user_notes JOIN users on user_id = edt_id where event_id = $1 AND (public=true OR (public=false AND user_id=$2))", [event_id, user_id], function(err, result){
                 query.getProviders()[provider].done();
                 if(err) {
                     return query.throwError(res);
