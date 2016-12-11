5535e5cb77833e2c7c191d4f8e323ffe7747be1a
Antoine Sauray <sauray.antoine@gmail.com>
2016-12-11 01:16:58 +0100

    fix

--- edt-query/post.js
+++ edt-query/post.js
@@ -83,11 +83,11 @@ module.exports = {
 						};
 						fcm.send(message)
   						.then(function(response){
-        					console.log("Successfully sent with response: ", response);
+        					//console.log("Successfully sent with response: ", response);
     					})
 						.catch(function(err){
-        					console.log("Something has gone wrong!");
-        					console.error(err);
+        					//console.log("Something has gone wrong!");
+        					//console.error(err);
 						});
 					}
 				});		
